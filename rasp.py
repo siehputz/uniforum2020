efec3ed438423b51edf0de55595a794981be1a15
siehputz <siehputz@gmail.com>
2020-01-21 15:25:21 -0600

    raspberry pi blinka version
    Note LED strip is on D18

---
+++ rasp.py
@@ -0,0 +1,20 @@
+import board
+import neopixel
+import time
+
+NUM_LEDS=50
+COLOR = (90,30,10)
+COLOR2 = (30,90,10)
+pixel = neopixel.NeoPixel(board.D18, 51, pixel_order=neopixel.GRB)
+x=range(NUM_LEDS)
+y=range(NUM_LEDS,1,-1)
+while True:
+    for a in x:
+        if a > 1:
+            pixel[a-1] = (0,0,0)
+            pixel[a] = COLOR
+            pixel[50-a] = (0,0,0)
+            pixel[49-a] = COLOR2
+        time.sleep(.05)
+    pixel[49] = (0, 0, 0)
+    pixel[0] = (0,0,0)

b292f8a7e16a83519046dfdb5bad6113084a7b14
siehputz <siehputz@gmail.com>
2020-01-21 16:17:51 -0600

    Added COLOR2

--- led2.py
+++ led2.py
@@ -4,6 +4,7 @@ import time
 
 NUM_LEDS=50
 COLOR = (90,30,10)
+COLOR2 = (30,90,10)
 pixel = neopixel.NeoPixel(board.A1, 51, pixel_order=neopixel.GRB)
 x=range(NUM_LEDS)
 y=range(NUM_LEDS,1,-1)
@@ -15,7 +16,7 @@ while True:
             pixel[a-1] = (0,0,0)
             pixel[a] = COLOR
             pixel[50-a] = (0,0,0)
-            pixel[49-a] = COLOR
+            pixel[49-a] = COLOR2
         time.sleep(.05)
     pixel[49] = (0, 0, 0)
     pixel[0] = (0,0,0)
